101c730066adeef787d4a982583da41a016a2feb
Lemuel Liverosk <ridoedee@gmail.com>
2017-07-08 18:32:49 -0700

    Clear Wording

--- magic_Square/app_To_Binary.py
+++ magic_Square/app_To_Binary.py
@@ -8,16 +8,16 @@ print('This program (supposedly) solves a 3-by-3 magic square\n\nverbose\tShow m
 # Initiate necessary values
 help_File = False
 # Prompt user for arguments
-verbose_Input = input('Do you want verbose? (\'y\' for yes and everything else for no): ')
-output_Input = input('Do you want output? (\'y\' for yes and everything else for no): ')
-complete_Input = input('Do you want all solutions? (\'y\' for yes and everything else for no): ')
+verbose_Input = input('Do you want verbose? (\'y\' for yes and anything else for no): ')
+output_Input = input('Do you want output? (\'y\' for yes and anything else for no): ')
+complete_Input = input('Do you want all solutions? (\'y\' for yes and anything else for no): ')
 # If there are arguments, proceed with arguments
-if verbose_Input in ['y', 'Y', '\'y\'', '\'Y\'', 'yes', 'Yes']:
+if verbose_Input.lower() in ['y', '\'y\'', 'yes']:
     value_Global.verbose = True
-if output_Input in ['y', 'Y', '\'y\'', '\'Y\'', 'yes', 'Yes']:
+if output_Input.lower() in ['y', '\'y\'', 'yes']:
     value_Global.output = True
     value_Global.output_File = open('Magic_Square_v2.log.txt', 'w')
-if complete_Input in ['y', 'Y', '\'y\'', '\'Y\'', 'yes', 'Yes']:
+if complete_Input.lower() in ['y', '\'y\'', 'yes']:
     value_Global.allsol = True
 verbose_Print('Calculation is initiating...\n\n')
 calculation()
